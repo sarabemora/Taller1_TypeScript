804e9764db60225c7fb36ef6210b747d26701326
Sara Benavides <s.benavidesm@uniandes.edu.co>
2023-10-21 16:17:49 -0500

    avance tabla

--- scripts/serie.js
+++ scripts/serie.js
@@ -1,2 +1,13 @@
-"use strict";
-console.log("Hola mundo");
+var Serie = /** @class */ (function () {
+    function Serie(numero, nombre, plataforma, temporadas, descripcion, link_plataforma, link_serie) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.plataforma = plataforma;
+        this.temporadas = temporadas;
+        this.descripcion = descripcion;
+        this.link_plataforma = link_plataforma;
+        this.link_serie = link_serie;
+    }
+    return Serie;
+}());
+export { Serie };
